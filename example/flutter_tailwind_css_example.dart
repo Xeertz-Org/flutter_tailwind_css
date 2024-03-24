507b44e51967f3645eaa5d494a8772c529b00ce5
mrpurpleknight <ziogaghi99@gmail.com>
2024-03-24 23:26:26 +0100

    initial commit

---
+++ example/flutter_tailwind_css_example.dart
@@ -0,0 +1,6 @@
+import 'package:flutter_tailwind_css/flutter_tailwind_css.dart';
+
+void main() {
+  var awesome = Awesome();
+  print('awesome: ${awesome.isAwesome}');
+}
---
+++ test/flutter_tailwind_css_test.dart
@@ -0,0 +1,16 @@
+import 'package:flutter_tailwind_css/flutter_tailwind_css.dart';
+import 'package:test/test.dart';
+
+void main() {
+  group('A group of tests', () {
+    final awesome = Awesome();
+
+    setUp(() {
+      // Additional setup goes here.
+    });
+
+    test('First Test', () {
+      expect(awesome.isAwesome, isTrue);
+    });
+  });
+}
