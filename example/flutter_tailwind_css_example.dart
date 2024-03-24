4f2b7420e4b8e790d671d19bd07a3a66d566e17e
mrpurpleknight <ziogaghi99@gmail.com>
2024-03-24 23:32:36 +0100

    published 0.0.1 (unlisted)

--- example/flutter_tailwind_css_example.dart
+++ example/flutter_tailwind_css_example.dart
@@ -1,4 +1,4 @@
-import 'package:flutter_tailwind_css/flutter_tailwind_css_framework.dart';
+import 'package:flutter_tailwind_css_framework/flutter_tailwind_css_framework.dart';
 
 void main() {
   var awesome = Awesome();
